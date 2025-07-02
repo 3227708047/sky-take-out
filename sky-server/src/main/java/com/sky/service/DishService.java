3c386063eacee500ed705a415fdca46634087143
3227708047 <17262150506@163.com>
2025-07-02 10:49:00 +0800

    员工分页查询、启用禁用员工账户、编辑员工、公共字段自动填充、新增菜品、菜品分页查询、删除菜品、修改菜品的业务开发

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,42 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface DishService {
+
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 批量删除菜品
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询菜品和对应的口味
+     * @param id
+     * @return
+     */
+    DishVO getByIdWithFlavor(Long id);
+
+    void updateWithFlavor(DishDTO dishDTO);
+}
