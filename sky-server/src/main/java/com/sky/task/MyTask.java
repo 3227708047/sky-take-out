4643983313e0569b109ccc8441bc745d7284b7eb
3227708047 <17262150506@163.com>
2025-07-19 09:58:54 +0800

    订单状态定时处理

---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,19 @@
+package com.sky.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class MyTask {
+    /**
+     * 定时任务 每隔5s 执行一次
+     */
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void executeTask(){
+        log.info("定时任务开始执行:{}",new Date());
+    }
+}
